837d2a55b3faa5593323d2151dbee1a7c56730d2
pecodrive <pecodrive@gmail.co>
2015-08-15 14:56:45 +0900

    add

---
+++ classes.php
@@ -0,0 +1,17 @@
+<?php
+require_once('index.php');
+class Data 
+{
+     private $url = 
+        'http://www.ted.com/'.
+        'talks/shawn_achor_the_happy_secret_to_better_work/'.
+        'transcript?language=en';
+     private $query = 
+        'body//div'.
+        '[@class="talk-article__body talk-transcript__body"]'.
+        '//span';
+     public function __get($name)
+     {
+         return $this->$name;
+     }
+}
